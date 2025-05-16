a48f425e61483451b9f3d12d39b7b91f58aedf7c
Adam <44596342+murdalok@users.noreply.github.com>
2025-05-16 13:21:18 -0400

    Update keydata.lua

--- keydata.lua
+++ keydata.lua
@@ -1,31 +1,26 @@
 -- SolaceGG.KeyData = {} defined in main.lua
 
-local function getUniqueName()
-   local name = UnitName("player")
-   local realm = GetRealmName()
-   return string.format("%s-%s", name, realm)
-end
-
-local player = getUniqueName()
-
-function SolaceGG.KeyData:SaveKey()
+function SolaceGG:SaveKey()
+   -- This method will write a new subtable [Player-Realm] to SolaceGG.KeyData
+   -- or update the table if it exists already
    local mapID = C_MythicPlus.GetOwnedKeystoneChallengeMapID()
    local keyLevel = C_MythicPlus.GetOwnedKeystoneLevel()
    if mapID and keyLevel then
       local mapName = C_ChallengeMode.GetMapUIInfo(mapID)
       local keydata = {}
-      keydata.character = player
+      keydata.character = SolaceGG.Player
       keydata.realm = GetRealmName()
       keydata.mapId = mapID
       keydata.mapName = mapName
       keydata.keyLevel = keyLevel
-      self[player] = keydata
+      SolaceSavedKey[SolaceGG.Player] = keydata
    end
 end
 
-function SolaceGG.KeyData:KeyRespond(msg)
-   if  self[player] and msg:lower():find("@keys") then
-      local response = string.format("%d - %s", self[player].keyLevel, self[player].mapName)
+function SolaceGG:KeyRespond(msg)
+   -- Responds to @keys in guild chat with the players current key, will respond regardless of sender
+   if SolaceSavedKey[SolaceGG.Player] and msg:lower():find("@keys") then
+      local response = string.format("%d - %s", SolaceSavedKey[SolaceGG.Player].keyLevel, SolaceSavedKey[SolaceGG.Player].mapName)
       SendChatMessage(response, "GUILD")
-    end
+   end
 end
